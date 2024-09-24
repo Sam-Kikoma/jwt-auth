e06032f6c2be6f87183dafd25890934b33718f6d
Sam Kikoma <kikomasamson@gmail.com>
2024-09-24 06:45:36 +0200

    Initial controller setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,38 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Auth user & get token
 // @route POST /api/users/auth
 // @access Public
-import asyncHandler from "express-async-handler";
-
 const authUser = (req, res) => {
 	res.json({ message: "Success" });
 };
 
-export { authUser };
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = (req, res) => {
+	res.json({ message: "Registered user" });
+};
+
+// @desc Logout user
+// @route POST /api/logout
+// @access Public
+const logoutUser = (req, res) => {
+	res.json({ message: "Logout user" });
+};
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = (req, res) => {
+	res.json({ message: "User Profile" });
+};
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+const updateUserProfile = (req, res) => {
+	res.json({ message: "Updated user profile" });
+};
+
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile };
