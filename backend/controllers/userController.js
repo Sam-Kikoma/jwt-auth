1752a583acc7a2208ff8fa4ae26c1fe47a008d48
Sam Kikoma <kikomasamson@gmail.com>
2024-09-26 21:25:31 +0200

    Updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,59 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @desc Auth user & get token
 // @route POST /api/users/auth
 // @access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
 	res.json({ message: "Success" });
-};
+});
 
 // @desc Register a new user
 // @route POST /api/users
 // @access Public
-const registerUser = (req, res) => {
-	res.json({ message: "Registered user" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+	const userExists = await User.findOne({ email });
+	if (userExists) {
+		res.status(400);
+		throw new Error("User already exists");
+	}
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
+});
 
 // @desc Logout user
 // @route POST /api/logout
 // @access Public
-const logoutUser = (req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
 	res.json({ message: "Logout user" });
-};
+});
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
-const getUserProfile = (req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
 	res.json({ message: "User Profile" });
-};
+});
 
 // @desc Update user profile
 // @route PUT /api/users/profile
 // @access Private
-const updateUserProfile = (req, res) => {
+const updateUserProfile = asyncHandler(async (req, res) => {
 	res.json({ message: "Updated user profile" });
-};
+});
 
 export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile };
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import connectDb from "./config/db.js";
 
 dotenv.config();
 
+connectDb();
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("It lives"));
 app.use("/api/users", userRoutes);
+
+// Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
