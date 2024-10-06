fbcc4b7b75a2cfeff770b74ea254702a388a48fa
Sam Kikoma <kikomasamson@gmail.com>
2024-10-06 12:27:00 +0200

    Added cookie parser middleware

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import connectDb from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-
 connectDb();
+
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => res.send("It lives"));
